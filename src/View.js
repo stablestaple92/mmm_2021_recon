cf105ca23fe0d794e14a9596ee431c9bb672e860
Lim Chang-Hyun <stablestaple92@gmail.com>
2022-04-07 17:15:52 +0900

    Revert "1.02"
    This reverts commit 9d968c44b5f3a531770d079fa0475ec7688cfa22.

--- src/View.js
+++ src/View.js
@@ -4,32 +4,26 @@ import "./View.css";
 const View = () => {
     return (
         <div>
-            
-            <div className="instagram block grayscale">
+            <section className="main__logo">
+            <img src="img/logo_mmm.png" alt="mmm_logo"/>
+            <h4>under re-construction...</h4>
+            </section>
+            <section className="button__container">
+            <div className="sns__line">
+                <a href="https://twitter.com/sany_on_" target="blank"><img src="img/logo_twitter_200.png" alt="twitter_logo" /></a>
                 <a href="https://www.instagram.com/sany_on_" target="blank"><img src="img/logo_insta_200.png" alt="instagram_logo" /></a>
-
-            </div>
-            <div className="youtube block grayscale">
                 <a href="https://www.youtube.com/channel/UCJ0B8Yk2-OygES2EPRoFgiw" target="blank"><img src="img/logo_youtube_200.png" alt="youtube_logo" /></a>
             </div>
-            <div className="twitter block grayscale">
-                <a href="https://twitter.com/sany_on_" target="blank"><img src="img/logo_twitter_200.png" alt="twitter_logo" /></a>
-            </div>
-            
-            <img className="logo__main block" src="img/logo_mmm.png" alt="mmm_logo"/>
-            <div className="soundcloud block grayscale">
+            <div className="music__line">
                 <a href="https://soundcloud.com/sany-on" target="blank"><img src="img/logo_sc_200.png" alt="soundcloud_logo" /></a>
-            </div>
-            <div className="bandcamp block grayscale">
                 <a href="https://sany-on.bandcamp.com/" target="blank"><img src="img/logo_bc_200.png" alt="bandcamp_logo" /></a>
-            </div>
-            <div className="mixlcoud block grayscale">
                 <a href="https://www.mixcloud.com/SANYON/" target="blank"><img src="img/logo_mix_200.png" alt="mixcloud_logo" /></a>
             </div>
-            <img className="logo__sd block" src="img/logo_mmm_sd.png" alt="mmm_sd_logo"/>
-            <div className="footer__block block">
-                <h5> &copy; {new Date().getFullYear()} MAD MIND MACHINE</h5>
-            </div>
+            </section>
+            <footer className="app__footer">
+            <img src="img/logo_mmm_sd.png" alt="mmm_sd_logo"/>
+            <h5> &copy; {new Date().getFullYear()} MAD MIND MACHINE</h5>
+            </footer>
         </div>
     );
 }
